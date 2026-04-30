1c82111a4d0fa50471e9568c6410056acd31ca0b
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2026-04-30 15:56:46 +0200

    Adds simple test cases for Boolean equivalence B

--- gipsl.all.build.equivalence.shortcut/src/gipsl/all/build/equivalence/shortcut/connector/EquivalenceShortcutConnector.java
+++ gipsl.all.build.equivalence.shortcut/src/gipsl/all/build/equivalence/shortcut/connector/EquivalenceShortcutConnector.java
@@ -1,23 +1,39 @@
 package gipsl.all.build.equivalence.shortcut.connector;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.emoflon.gips.core.milp.SolverOutput;
 
+import gipsl.all.build.equivalence.shortcut.api.gips.ShortcutGipsAPI;
+import gipsl.all.build.equivalence.shortcut.api.gips.mapping.N2nMapping;
 import test.suite.gips.utils.AConnector;
+import test.suite.gips.utils.GipsTestUtils;
 import test.suite.gips.utils.GlobalTestConfig;
 
 public class EquivalenceShortcutConnector extends AConnector {
 
 	public EquivalenceShortcutConnector(final String modelPath) {
-//		api = new ShortcutGipsAPI();
-//		api.init(GipsTestUtils.pathToAbsUri(modelPath));
+		api = new ShortcutGipsAPI();
+		api.init(GipsTestUtils.pathToAbsUri(modelPath));
 		GlobalTestConfig.overrideSolver(api);
 	}
 
 	@Override
 	public SolverOutput run(String outputPath) {
 		final SolverOutput output = solve();
-//		((ShortcutGipsAPI) api).getN2n().applyNonZeroMappings();
+		((ShortcutGipsAPI) api).getN2n().applyNonZeroMappings();
 		return output;
+	}
+	
+	public int getY() {
+		final List<N2nMapping> selectedMappingsWithY1 = ((ShortcutGipsAPI) api).getN2n().getMappings().values().stream()
+				.filter(m -> m.getAdditionalVariables().get("y").getValue().intValue() == 1)
+				.collect(Collectors.toList());
+		assert selectedMappingsWithY1.size() == 1
+				: "Number of mapping instances is not equal to 1. Value: " + selectedMappingsWithY1.size();
+
+		return selectedMappingsWithY1.get(0).getAdditionalVariables().get("y").getValue().intValue();
 	}
 
 }
---
+++ test.suite.gips/src/test/suite/gipsl/all/build/GipslAllBuildEquivalenceShortcutTest.java
@@ -0,0 +1,91 @@
+package test.suite.gipsl.all.build;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.emoflon.gips.core.milp.SolverOutput;
+import org.emoflon.gips.core.milp.SolverStatus;
+import org.junit.jupiter.api.Test;
+
+import gipsl.all.build.equivalence.shortcut.connector.EquivalenceShortcutConnector;
+
+public class GipslAllBuildEquivalenceShortcutTest extends AGipslAllBuildTest {
+
+	// Setup method
+
+	public void callableSetUp() {
+		gen.persistModel(MODEL_PATH);
+		con = new EquivalenceShortcutConnector(MODEL_PATH);
+	}
+
+	// Actual tests
+	// Positive tests
+
+	@Test
+	public void testMap1to1() {
+		gen.genSubstrateNode("s1", 2);
+		gen.genVirtualNode("v1", 7);
+		callableSetUp();
+
+		final SolverOutput ret = con.run(OUTPUT_PATH);
+
+		assertEquals(SolverStatus.OPTIMAL, ret.status());
+		assertEquals(1.1, Math.abs(ret.objectiveValue()));
+		assertEquals(1, ((EquivalenceShortcutConnector) con).getY());
+	}
+
+	@Test
+	public void testMap2to1() {
+		gen.genSubstrateNode("s1", 2);
+		gen.genVirtualNode("v1", 7);
+		gen.genVirtualNode("v2", 42);
+		callableSetUp();
+
+		final SolverOutput ret = con.run(OUTPUT_PATH);
+
+		assertEquals(SolverStatus.OPTIMAL, ret.status());
+		assertEquals(1.1, Math.abs(ret.objectiveValue()));
+		assertEquals(1, ((EquivalenceShortcutConnector) con).getY());
+	}
+
+	@Test
+	public void testMap8to42() {
+		gen.genSubstrateNode("s1", 42);
+		gen.genVirtualNode("v1", 8);
+		callableSetUp();
+
+		final SolverOutput ret = con.run(OUTPUT_PATH);
+
+		assertEquals(SolverStatus.OPTIMAL, ret.status());
+		assertEquals(0, Math.abs(ret.objectiveValue()));
+	}
+
+	// Negative tests
+
+	@Test
+	public void testMap0to1() {
+		gen.genSubstrateNode("s1", 2);
+		callableSetUp();
+
+		final SolverOutput ret = con.run(OUTPUT_PATH);
+
+		assertEquals(SolverStatus.OPTIMAL, ret.status());
+		assertEquals(0, Math.abs(ret.objectiveValue()));
+	}
+
+	@Test
+	public void testMap1to0() {
+		gen.genVirtualNode("v1", 1);
+		callableSetUp();
+
+		final SolverOutput ret = con.run(OUTPUT_PATH);
+
+		assertEquals(SolverStatus.OPTIMAL, ret.status());
+		assertEquals(0, Math.abs(ret.objectiveValue()));
+	}
+
+	@Override
+	public Class<?> getConnectorClass() {
+		return EquivalenceShortcutConnector.class;
+	}
+
+}
