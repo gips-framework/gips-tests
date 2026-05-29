ab1af93cbf1b08508ce29fc800c4450d542d7fea
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2026-05-29 09:20:27 +0200

    Adds basic tests to check the implicit Boolean feature

---
+++ gipsl.all.build.implicitboolean/src/gipsl/all/build/implicitboolean/connector/ImplicitBooleanConnector.java
@@ -0,0 +1,42 @@
+package gipsl.all.build.implicitboolean.connector;
+
+import java.util.Objects;
+
+import org.emoflon.gips.core.milp.SolverOutput;
+
+import gipsl.all.build.implicitboolean.api.gips.ImplicitbooleanGipsAPI;
+import model.VirtualResourceNode;
+import test.suite.gips.utils.AConnector;
+import test.suite.gips.utils.GipsTestUtils;
+import test.suite.gips.utils.GlobalTestConfig;
+
+public class ImplicitBooleanConnector extends AConnector {
+
+	public ImplicitBooleanConnector(final String modelPath) {
+		api = new ImplicitbooleanGipsAPI();
+		api.init(GipsTestUtils.pathToAbsUri(modelPath));
+		GlobalTestConfig.overrideSolver(api);
+	}
+
+	@Override
+	public SolverOutput run(final String outputPath) {
+		final SolverOutput output = solve();
+		((ImplicitbooleanGipsAPI) api).getN2n().applyNonZeroMappings();
+		return output;
+	}
+
+	public int getVirtualResourceNodeS(final String name) {
+		Objects.requireNonNull(name);
+
+		final var t = api.getTypeExtensions()
+				.get("platform:/resource/gipsl.all.build.model/model/Model.ecore/VirtualResourceNode");
+
+		for (final var e : t.getExtensions()) {
+			if (((VirtualResourceNode) e.getContext()).getName().equals(name)) {
+				return (int) e.getVariables().get("s").getValue();
+			}
+		}
+		throw new UnsupportedOperationException();
+	}
+
+}
